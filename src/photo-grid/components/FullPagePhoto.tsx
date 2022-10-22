53a3c3aeb75ff93d429a58aaf18bc8222fdc58fb
Alex Aritan <cokethenwpepsi@gmail.com>
2022-10-22 12:05:44 -0400

    support optional full page photo

--- src/photo-grid/components/FullPagePhoto.tsx
+++ src/photo-grid/components/FullPagePhoto.tsx
@@ -4,18 +4,37 @@ import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import { IconButton } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { useSwipeable } from 'react-swipeable';
+import { SwipeableHandlers, useSwipeable } from 'react-swipeable';
 import { useKeyPress } from '../utils/hooks/useKeyPress';
+
+const renderFullPage = ({
+	children,
+	fullScreen,
+	swipeHandlers,
+}: {
+	children: React.ReactElement;
+	fullScreen: boolean;
+	swipeHandlers: SwipeableHandlers;
+}) =>
+	fullScreen ? (
+		<Dialog fullScreen open={true} {...swipeHandlers}>
+			{children}
+		</Dialog>
+	) : (
+		<div {...swipeHandlers}>{children}</div>
+	);
 
 export const FullPagePhoto = ({
 	handleClose,
 	handleNext,
 	handlePrevious,
+	fullScreen,
 	src,
 }: {
 	handleClose: () => void;
 	handleNext: () => void;
 	handlePrevious: () => void;
+	fullScreen: boolean;
 	src: string;
 }) => {
 	//Get info about the theme.
@@ -34,49 +53,67 @@ export const FullPagePhoto = ({
 		preventDefaultTouchmoveEvent: true,
 	});
 
-	return (
-		<Dialog fullScreen open={true} {...swipeHandlers}>
-			{shouldRenderNavIcons ? (
-				<IconButton
-					edge="start"
-					onClick={handleClose}
-					style={{ left: 20, position: 'absolute', top: 10 }}
-				>
-					<CloseIcon />
-				</IconButton>
-			) : null}
-			{src ? (
-				<img
-					alt=""
-					src={src}
-					style={{
-						maxHeight: '100%',
-						maxWidth: '100%',
-						objectFit: 'contain',
-					}}
-				/>
-			) : null}
+	return renderFullPage({
+		children: (
+			<>
+				{shouldRenderNavIcons ? (
+					<IconButton
+						edge="start"
+						onClick={handleClose}
+						style={{
+							left: 20,
+							position: 'absolute',
+							top: 10,
+							zIndex: 10000,
+							...(!fullScreen && { top: '90px' }),
+						}}
+					>
+						<CloseIcon />
+					</IconButton>
+				) : null}
+				{src ? (
+					<img
+						alt=""
+						src={src}
+						style={{
+							...(fullScreen && { margin: 'auto' }),
+							...(!fullScreen && {
+								left: 0,
+								marginLeft: 'auto',
+								marginRight: 'auto',
+								maxHeight: '85%',
+								position: 'absolute',
+								right: 0,
+							}),
+							maxWidth: '100%',
+							objectFit: 'contain',
+						}}
+					/>
+				) : null}
 
-			{shouldRenderNavIcons ? (
-				<IconButton
-					edge="start"
-					onClick={handlePrevious}
-					size="large"
-					style={{ left: 20, position: 'absolute', top: '47%' }}
-				>
-					<NavigateBeforeIcon />
-				</IconButton>
-			) : null}
-			{shouldRenderNavIcons ? (
-				<IconButton
-					edge="start"
-					onClick={handleNext}
-					size="large"
-					style={{ right: 20, position: 'absolute', top: '47%' }}
-				>
-					<NavigateNextIcon />
-				</IconButton>
-			) : null}
-		</Dialog>
-	);
+				{shouldRenderNavIcons ? (
+					<IconButton
+						edge="start"
+						onClick={handlePrevious}
+						size="large"
+						style={{ left: 20, position: 'absolute', top: '47%' }}
+					>
+						<NavigateBeforeIcon />
+					</IconButton>
+				) : null}
+				{shouldRenderNavIcons ? (
+					<IconButton
+						edge="start"
+						onClick={handleNext}
+						size="large"
+						style={{ right: 20, position: 'absolute', top: '47%' }}
+					>
+						<NavigateNextIcon />
+					</IconButton>
+				) : null}
+			</>
+		),
+		fullScreen,
+		swipeHandlers,
+	});
 };
